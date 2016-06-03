34cfb2ce2c03c25d400da3fbfea4309a5a5e59e9
Silwer0k <egor.yakubovich@ictis.sfedu.ru>
2016-06-03 15:20:25 +0300

    Комментарии в файле Series.cpp
    Добавление комментариев для функций

--- Series.cpp
+++ Series.cpp
@@ -8,28 +8,28 @@ public:
 
 	void AddFigure(char ID)
 	{
-		_figures.push_back(FacFig.CreateFigure(ID));
+		_figures.push_back(FacFig.CreateFigure(ID));//создание фигуры заданного типа
 	}
 
-	void SetFigureInfo()
+	void SetFigureInfo()//установка для данной фигуры ее параметров
 	{
 		int index = _figures.size() - 1;
 		_figures[index]->SetCharacteristic(index);
 	}
 
-	double GetAreaOneFigure(int n) const
+	double GetAreaOneFigure(int n) const //получение площади фигуры
 	{
 		int inx = n - 1;
 		return _figures[inx]->GetArea();
 	}
 
-	double GetVolumeOneFigure(int n) const
+	double GetVolumeOneFigure(int n) const//получение объема фигуры
 	{
 		int inx = n - 1;
 		return _figures[inx]->GetVolume();
 	}
 
-	char GetFigureID(int inx)const
+	char GetFigureID(int inx)const//получение ID фигуры P – параллелепипед, C – конус, S – сфера, R - пирамида
 	{
 		return _figures[inx]->GetId();
 	}
